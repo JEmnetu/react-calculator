b450a5282186ee7991bc69cea22e3fece5fcfe3b
JEmnetu <j.habtema@gmail.com>
2020-08-31 23:31:38 -0400

    gitignore update

---
+++ build/precache-manifest.f3542a7a5a69e1466ea0937d9fb744b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f86cc77065650359089c5e2cc51b9b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83830ec86c5bbcfbfa50",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7381705ca11c97b948da",
+    "url": "/static/css/main.2abd70f3.chunk.css"
+  },
+  {
+    "revision": "83830ec86c5bbcfbfa50",
+    "url": "/static/js/2.1e55130c.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/static/js/2.1e55130c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7381705ca11c97b948da",
+    "url": "/static/js/main.03d80836.chunk.js"
+  },
+  {
+    "revision": "5738b0f7c428695a68f5",
+    "url": "/static/js/runtime-main.2e17527d.js"
+  }
+]);
